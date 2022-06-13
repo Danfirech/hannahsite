59b216fb6203ea9b611d28d410a0261907fd124e
Dan Possehl <danielbpossehl@gmail.com>
2022-06-13 12:52:18 -0700

    nat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const FooterMain = styled.div`
   height: 100px;
   width: 100%;
   display: flex;
   background-color: pink;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Footer = () => {
   return (
     <>
-      <FooterMain>Contact Info</FooterMain>
+      <FooterMain>
+        <FacebookIcon />
+        <InstagramIcon />
+      </FooterMain>
     </>
   );
 };
